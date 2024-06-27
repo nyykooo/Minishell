57385357e74120c08de5596c32165cab6da5089e
nyykooo <nicholas.cakla@outlook.com>
2024-06-27 15:48:47 +0100

    fixing exit error messages and verifications

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ncampbel <ncampbel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 14:30:52 by ncampbel          #+#    #+#             */
-/*   Updated: 2024/06/27 10:23:46 by ncampbel         ###   ########.fr       */
+/*   Updated: 2024/06/27 10:46:33 by ncampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,7 +77,7 @@ void	minishell_loop(t_minishell *shell)
 		if (create_prompt(shell))
 		{
 			analyze_input(shell);
-			// update_questionvar(shell);
+			update_questionvar(shell);
 			clear_shell(shell);
 		}
 	}
