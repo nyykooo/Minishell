82b0850e22c6cf2e2314f9b2ca0766eaf81ecc30
gitbrunoh <brunohenriqueufpr@gmail.com>
2024-08-07 19:18:03 +0100

    correcting the SIGINT behavior inside child processes

--- src/pipe_redir/handle_pipe_redir.c
+++ src/pipe_redir/handle_pipe_redir.c
@@ -6,7 +6,7 @@
 /*   By: brunhenr <brunhenr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 15:59:53 by brunhenr          #+#    #+#             */
-/*   Updated: 2024/08/05 17:22:01 by brunhenr         ###   ########.fr       */
+/*   Updated: 2024/08/07 19:14:36 by brunhenr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,22 +31,11 @@ static void	ft_close_pipefds(int fd[2], int old_read_fd)
 		close(fd[0]);
 }
 
-static void	ft_wait_all_children(void)
-{
-	int	waitpid_return;
-
-	waitpid_return = 0;
-	while (waitpid_return != -1)
-	{
-		waitpid_return = waitpid(-1, NULL, WNOHANG);
-	}
-}
-
 static void	analyze_status(int status, t_minishell *shell)
 {
 	if (WIFEXITED(status))
 		shell->exit_status = WEXITSTATUS(status);
-	else if (WIFSIGNALED(status) || WIFSTOPPED(status))
+	if (WIFSIGNALED(status) || WIFSTOPPED(status))
 	{
 		if (WTERMSIG(status) == SIGQUIT)
 		{
@@ -61,6 +50,33 @@ static void	analyze_status(int status, t_minishell *shell)
 	}
 }
 
+static void	ft_wait_all_children(t_minishell *shell)
+{
+	int	waitpid_return;
+	int	status;
+
+	waitpid_return = 0;
+	while (waitpid_return != -1)
+	{
+		waitpid_return = waitpid(-1, &status, 0);
+		if (waitpid_return > 0)
+		{
+			if (WIFEXITED(status))
+				continue ;
+			else if (WIFSIGNALED(status))
+			{
+				if (WTERMSIG(status) == SIGQUIT)
+					shell->exit_status = 131;
+				else if (WTERMSIG(status) == SIGINT)
+				{
+					shell->exit_status = 130;
+					printf("\n");
+				}
+			}
+		}
+	}
+}
+
 void	handle_pipe_and_redir(t_minishell *shell, t_cmd *commands)
 {
 	int		fd[2];
@@ -70,7 +86,7 @@ void	handle_pipe_and_redir(t_minishell *shell, t_cmd *commands)
 	ignore_some_signals();
 	old_read_fd = 0;
 	status = ft_pipe_redir_loop(shell, commands, fd, old_read_fd);
-	ft_wait_all_children();
+	ft_wait_all_children(shell);
 	analyze_status(status, shell);
 	ft_close_pipefds(fd, old_read_fd);
 	config_signals(0);
