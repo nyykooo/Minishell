c30613bc4569ba6add4e7c877a8fe99b2929eb2e
gitbrunoh <brunohenriqueufpr@gmail.com>
2024-08-12 10:52:10 +0100

    tester_1 ok - making a stable version again

--- src/heredoc/heredoc.c
+++ src/heredoc/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: brunhenr <brunhenr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 09:52:35 by brunhenr          #+#    #+#             */
-/*   Updated: 2024/08/09 12:44:32 by brunhenr         ###   ########.fr       */
+/*   Updated: 2024/08/12 10:34:17 by brunhenr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,25 +22,25 @@ void	config_ignore_sigint(struct sigaction *sa_original)
 	sigaction(SIGINT, &sa_ignore, sa_original);
 }
 
-void	initialize_heredoc(struct sigaction *sa_original)
+void	initialize_heredoc(struct termios *term, struct sigaction *sa_original)
 {
 	config_ignore_sigint(sa_original);
-	//tcgetattr(STDIN_FILENO, term);
+	tcgetattr(STDIN_FILENO, term);
 }
 
-void	finalize_heredoc(struct sigaction *sa_original)
+void	finalize_heredoc(struct termios *term, struct sigaction *sa_original)
 {
 	if (sigaction(SIGINT, sa_original, NULL) == -1)
 	{
 		perror("sigaction");
 		exit(1);
 	}
-	//tcsetattr(STDIN_FILENO, TCSANOW, term);
+	tcsetattr(STDIN_FILENO, TCSANOW, term);
 }
 
 int	heredoc(t_minishell *shell)
 {
-	//struct termios		term;
+	struct termios		term;
 	struct sigaction	sa_original;
 	int					status;
 
